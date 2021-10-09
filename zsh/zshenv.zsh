1bf0279fd446b323a14c2eecd1882dd23fd01104
Artin Mobasher(artart222) <mobasherartin.icm@gmail.com>
2021-10-10 00:50:56 +0330

    Add some new zsh files and move all zsh files to single directory

---
+++ zsh/zshenv.zsh
@@ -0,0 +1,5 @@
+ZDOTDIR=$HOME/.config/zsh
+_Z_DATA=$HOME/.cache/zsh/z
+GIT_CONFIG=$XDG_CONFIG_HOME/git/config
+export XAUTHORITY="$XDG_RUNTIME_DIR"/Xauthority
+CARGO_HOME=$XDG_CONFIG_HOME/rust/cargo
