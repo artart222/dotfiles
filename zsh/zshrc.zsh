7bffea3a2cedaa6ce53cc5c489a423892246a485
Artin Mobasher(artart222) <mobasherartin.icm@gmail.com>
2021-10-16 11:31:46 +0330

    This is another file that Fixed tray position. forgot to put it in correct commit

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -138,3 +138,5 @@ export LESSHISTFILE=$HOME/.config/less/lesshst
 
 # To customize prompt, run `p10k configure` or edit ~/.config/zsh/.p10k.zsh.
 [[ ! -f ~/.config/zsh/p10k.zsh ]] || source ~/.config/zsh/p10k.zsh
+
+source ~/dotfiles/scripts/tray.sh
