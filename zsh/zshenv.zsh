be177b68f572a855ea6902c32b799643eb0a6ed8
Artin Mobasher(artart222) <mobasherartin.icm@gmail.com>
2022-04-11 01:27:31 +0430

    File updated

--- zsh/zshenv.zsh
+++ zsh/zshenv.zsh
@@ -5,11 +5,14 @@ XDG_CACHE_HOME=$HOME/.cache
 ZDOTDIR=$XDG_CONFIG_HOME/zsh
 _Z_DATA=$XDG_CACHE_HOME/zsh/z
 
+# Set display environmental variable
+export DISPLAY=:0
+
 # Loading other zsh files
 if [ -r $ZDOTDIR/zshrc.zsh ]; then
   source $ZDOTDIR/zshrc.zsh
 fi
 
-if [ -r $ZDOTDIR/zprofile.zsh ]; then
-  source $ZDOTDIR/zprofile.zsh
-fi
+# if [ -r $ZDOTDIR/zprofile.zsh ]; then
+#   source $ZDOTDIR/zprofile.zsh
+# fi
