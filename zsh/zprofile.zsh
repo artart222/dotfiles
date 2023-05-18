a9504fbcc47ed7a83d9a315abfb6e54f0b04c558
Artin Mobasher(artart222) <mobasherartin.icm@gmail.com>
2023-05-19 02:57:25 +0330

    Autorun hyprland

--- zsh/zprofile.zsh
+++ zsh/zprofile.zsh
@@ -1,3 +1,4 @@
-if [ -z "${DISPLAY}" ] && [ "${XDG_VTNR}" -le 3 ]; then
-  exec startx
+if [ -z "${DISPLAY}" ] && [ "${XDG_VTNR}" -eq 1 ]; then
+  unset DISPLAY
+  exec Hyprland
 fi
--- zsh/zshenv.zsh
+++ zsh/zshenv.zsh
@@ -6,13 +6,13 @@ ZDOTDIR=$XDG_CONFIG_HOME/zsh
 _Z_DATA=$XDG_CACHE_HOME/zsh/z
 
 # Set display environmental variable
-export DISPLAY=:0
+# export DISPLAY=:0
 
 # Loading other zsh files
 if [ -r $ZDOTDIR/zshrc.zsh ]; then
   source $ZDOTDIR/zshrc.zsh
 fi
 
-# if [ -r $ZDOTDIR/zprofile.zsh ]; then
-#   source $ZDOTDIR/zprofile.zsh
-# fi
+if [ -r $ZDOTDIR/zprofile.zsh ]; then
+  source $ZDOTDIR/zprofile.zsh
+fi
