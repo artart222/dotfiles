5e4d2405514ea2b11c718e273a4819baa4109a0b
Artin Mobasher(artart222) <mobasherartin.icm@gmail.com>
2023-07-13 03:32:09 +0330

    Some cleaning

--- zsh/zshenv.zsh
+++ zsh/zshenv.zsh
@@ -1,3 +1,4 @@
+# Set xdg config and xdg cache environmental variables
 XDG_CONFIG_HOME=$HOME/.config
 XDG_CACHE_HOME=$HOME/.cache
 
@@ -6,13 +7,12 @@ ZDOTDIR=$XDG_CONFIG_HOME/zsh
 _Z_DATA=$XDG_CACHE_HOME/zsh/z
 
 # Set display environmental variable
-# export DISPLAY=:0
+export DISPLAY=:0
 
 # Loading other zsh files
 if [ -r $ZDOTDIR/zshrc.zsh ]; then
   source $ZDOTDIR/zshrc.zsh
 fi
-
 if [ -r $ZDOTDIR/zprofile.zsh ]; then
   source $ZDOTDIR/zprofile.zsh
 fi
